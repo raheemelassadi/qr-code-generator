00e77ff3f7bfa1f1b0d317c0636e0a9a10de941a
raheemelassadi <raheemelassadi1@gmail.com>
2023-07-02 04:27:36 -0700

    made it so qr code changes on enter or button click, google is first qr code on load

--- main.js
+++ main.js
@@ -1,14 +1,31 @@
 document.addEventListener('DOMContentLoaded', function() {
+    const qrForm = document.querySelector('#qr-form');
     const userInput = document.querySelector('#user-input');
-    const qrCodeDiv = document.getElementById('qr-code');
-    
-    userInput.addEventListener('input', function() {
-      const value = userInput.value;
-      qrCodeDiv.innerHTML = ''; 
-      
+    const qrCodeDiv = document.querySelector('#qr-code');
+  
+    const generateQRCode = function(value) {
+      qrCodeDiv.innerHTML = ''; // Clear previous QR code
+  
       if (value) {
         new QRCode(qrCodeDiv, value);
       }
+    };
+  
+    qrForm.addEventListener('submit', function(event) {
+      event.preventDefault(); // Prevent form submission
+      const value = userInput.value;
+      generateQRCode(value);
     });
+  
+    userInput.addEventListener('keydown', function(event) {
+      if (event.key === 'Enter') {
+        event.preventDefault(); // Prevent form submission
+        const value = userInput.value;
+        generateQRCode(value);
+      }
+    });
+  
+    // Generate initial QR code on page load
+    generateQRCode('https://www.google.com');
   });
   
